a5b16e71a3329380e2faed8f214e62127a5f665f
Nathan Avaaga <nathanavaaga@gmail.com>
2024-05-17 13:01:25 +0300

    Learnt conversion characters

---
+++ Shenanigans/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+This is my program
+it prints out my name
+it is pretty awesome
+*/
+
+int main()
+{
+        //These lines of code print my name
+        printf("Nate is awesome\n");
+        printf("Nate is cool\n");
+
+        //These lines show implementation of conversion characters
+
+        //String
+        printf("%s is the best thing ever\n", "Tech");
+
+        //Integer
+        printf("I ate %d eggs last night\n", 12);
+
+        //Float
+        printf("Pi is equal to %.2f\n", 3.1415926);
+        printf("Pi is equal to %.4f\n", 3.1415926);
+        printf("Pi is equal to %f\n", 3.1415926);
+
+
+
+        return 0;
+}
